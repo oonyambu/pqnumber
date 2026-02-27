6ed3ee2c4d2ec3ec4316ae0e4ed811b4c348151d
oonyambu <oonyambu@gmail.com>
2026-02-27 01:27:15 -0800

    Rcpp Evalcpp

--- R/pqnumber.R
+++ R/pqnumber.R
@@ -1,10 +1,13 @@
 #' @useDynLib pqnumber, .registration = TRUE
 NULL
 
+#' @importFrom Rcpp evalCpp
+NULL
+
 #' Print a pqnumber in a good manner
 #' @param x a pqnumber
 #'
 #' @export
-print.pqnumber <- function(x, digits = getOption('digits'), ...){
+print.pqnumber <- function(x, digits = getOption("digits"), ...) {
   cat(do.call(my_format, x))
 }
